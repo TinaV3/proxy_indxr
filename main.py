3ce3f1b61410ca5e5a0c822e6c304ba93f8b69b6
cuturilo-malkier <cuturilo.malkier@gmail.com>
2026-02-05 11:23:19 +0100

    Added main.py script

---
+++ main.py
@@ -0,0 +1,18 @@
+from proxy_server import run_proxy
+
+if __name__ == "__main__":
+    from cfg.config_model import NativeHostConfig
+
+    # TEMP hardcoded config for testing
+    config = NativeHostConfig(
+        local_proxy_host="127.0.0.1",
+        local_proxy_port=1389,
+    )
+
+    run_proxy(
+        remote_proxy_host="de-1.px.indexr.ai",
+        remote_proxy_port=3129,
+        proxy_username="proxy-checker",
+        proxy_password="taisharmalkier",
+        config=config
+    )
